f25bc6c20ce6b68472d2086136fdc2d9a65e0cd6
student <student@PC111S02.fgakusei.local>
2014-06-06 11:15:27 +0900

    画面遷移

---
+++ src/jp/ac/st/originalaso_2014_002/HitokotoActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.st.originalaso_2014_002;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HitokotoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.hitokotoactivity);
+	}
+}
---
+++ src/jp/ac/st/originalaso_2014_002/MaintenanceActivity.java
@@ -0,0 +1,17 @@
+package jp.ac.st.originalaso_2014_002;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MaintenanceActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.maintenanceactivity);
+	}
+	
+	
+	
+}
