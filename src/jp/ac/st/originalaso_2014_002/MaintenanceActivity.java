7a2642aa8e0cfaccafcf33d4c27c5d5079c07d67
student <student@PC111S02.fgakusei.local>
2014-06-25 14:41:05 +0900

    完成!!

--- src/jp/ac/st/originalaso_2014_002/MaintenanceActivity.java
+++ src/jp/ac/st/originalaso_2014_002/MaintenanceActivity.java
@@ -11,18 +11,19 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class MaintenanceActivity extends Activity implements View.OnClickListener, AdapterView.OnItemClickListener{
 
 	SQLiteDatabase sdb = null;
 	MySQLiteOpenHelper helper = null;
-	
+
 	int selectedID = -1;
 	int lastPosition = -1;
-	
-	
-	
-	
+
+
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO 自動生成されたメソッド・スタブ
@@ -36,7 +37,14 @@ public class MaintenanceActivity extends Activity implements View.OnClickListene
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+		if(this.selectedID!=-1){
+			parent.getChildAt(this.lastPosition).setBackgroundColor(0);
+		}
+		view.setBackgroundColor(android.graphics.Color.LTGRAY);
+
+		SQLiteCursor cursor = (SQLiteCursor)parent.getItemAtPosition(position);
+		this.selectedID = cursor.getInt(cursor.getColumnIndex("_id"));
+		this.lastPosition = position;
 	}
 
 
@@ -45,23 +53,23 @@ public class MaintenanceActivity extends Activity implements View.OnClickListene
 	protected void onResume() {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onResume();
-		
+
 		Button btnD = (Button)findViewById(R.id.btnDELETE);
 		Button btnB = (Button)findViewById(R.id.btnback);
 		ListView lstH = (ListView)findViewById(R.id.lsthitokoto);
 
-		btnD.setOnClickListener(this);	
+		btnD.setOnClickListener(this);
 		btnB.setOnClickListener(this);
-		
+
 		lstH.setOnItemClickListener(this);
-		
+
 		this.setDBValuetoList(lstH);
 	}
 
 	private void setDBValuetoList(ListView lstH) {
 		// TODO 自動生成されたメソッド・スタブ
 		SQLiteCursor cursor = null;
-		
+
 		if(sdb == null) {
 			helper = new MySQLiteOpenHelper(getApplicationContext());
 		}
@@ -71,20 +79,50 @@ public class MaintenanceActivity extends Activity implements View.OnClickListene
 			Log.e("ERROR",e.toString());
 		}
 		cursor = this.helper.selectHitokotoList(sdb);
-		
+
 		int db_layout = android.R.layout.simple_list_item_activated_1;
-		String[]from = {"pharse"};
+		String[]from = {"phrase"};
 		int[] to = new int[]{android.R.id.text1};
-		
+
 		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,db_layout,cursor,from,to,0);
-		
+
 		lstH.setAdapter(adapter);
 	}
 
+	private void deleteFromHitokoto(int id){
+		if(sdb == null)	{
+			helper = new MySQLiteOpenHelper(getApplicationContext());
+		}
+		try{
+			sdb = helper.getWritableDatabase();
+		}catch(SQLiteException e){
+			Log.e("ERROR", e.toString());
+		}
+		this.helper.deleteHitokoto(sdb,  id);
+	}
+	
 	@Override
 	public void onClick(View v) {
-		// TODO 自動生成されたメソッド・スタブ
-		
+		switch(v.getId()){
+			case R.id.btnDELETE:
+	
+				if(this.selectedID != -1){
+					this.deleteFromHitokoto(this.selectedID);
+					ListView lstH  = (ListView)findViewById(R.id.lsthitokoto);
+					this.setDBValuetoList(lstH);
+					this.selectedID = -1;
+					this.lastPosition = -1;
+				}else{
+					Toast.makeText(MaintenanceActivity.this, "消去する行を選んでください",Toast.LENGTH_SHORT).show();
+				}
+			break;
+			case R.id.btnback:
+				finish();
+			break;
+			// TODO 自動生成されたメソッド・スタブ
+		}
 	}
 
 }
+
+
